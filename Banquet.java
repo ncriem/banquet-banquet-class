1f2fac3a4d17206c9753f50d7c5dfb967fe74df9
Joshua-Payne <48285801+Joshua-Payne@users.noreply.github.com>
2019-04-17 08:34:15 -0400

    Add files via upload

---
+++ Banquet.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Banquet {
+
+    ArrayList<String> companies = new ArrayList<>(Arrays.asList("WalMart","Kroger","Amazon","Lowes","BestWestern","KMart","Fusian","Heinz",
+            "Gucci","Prada","Nike","Dodge","Maserati","Razor","AMD","Razer"));
+    ArrayList<Attendee> attendees = new ArrayList<>();
+    ArrayList<int> companiescount = new ArrayList<>(companies.size());
+    int aIDcounter = 0;
+
+    public void register() {
+        if(aIDcounter<100){
+            Scanner scan = new Scanner();
+            int company = scan.nextInt();
+            if(companiescount.get(company)<10) {
+                String first = scan.next();
+                String last = scan.next();
+                String allergies = scan.nextLine();
+                int aID = aIDcounter;
+                attendees.add(new Attendee(first, last, company, allergies, aID));
+                aIDcounter++;
+            }
+            else System.out.println("Each Company can only submit 10 attendees");
+        }
+        else System.out.println("REGISTRATION CLOSED");
+    }
+}
