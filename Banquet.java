f49d00af68cf2849f35a812350341936c2f3fa32
Joshua-Payne <48285801+Joshua-Payne@users.noreply.github.com>
2019-04-18 09:02:43 -0400

    Add files via upload
    Updated banquet class with completed register method

--- Banquet.java
+++ Banquet.java
@@ -5,44 +5,71 @@ import java.util.Scanner;
 public class Banquet {
 
     // companies is an arraylist of every company in attendance. their comanyID is their index in the arrayList
-    ArrayList<String> companies = new ArrayList<>(Arrays.asList("WalMart","Kroger","Amazon","Lowes","BestWestern","KMart","Fusian","Heinz",
-            "Gucci","Prada","Nike","Dodge","Maserati","Razor","AMD","Razer"));
+    ArrayList<String> companies = new ArrayList<>(Arrays.asList("WalMart", "Kroger", "Amazon", "Lowes", "BestWestern", "KMart", "Fusian", "Heinz",
+            "Gucci", "Prada", "Nike", "Dodge", "Maserati", "Razor", "AMD", "Razer"));
 
     //an arraylistof 100 attendees
     ArrayList<Attendee> attendees = new ArrayList<>();
 
-    //an arraylist that tracks the number of attendees sent by each company
-    ArrayList<Integer> companiescount = new ArrayList<>(companies.size());
+    //an array that tracks the number of attendees sent by each company
+    int[] companiescounter = new int[companies.size()];
 
-    //attendeeID 0-99 incremented by the register method
+    //attendeeID 0-99 incremented by the register method. allows attendees to be found within the 100 attendee arraylist
     int aIDcounter = 0;
 
-    //adds a new company to the ArrayList
-    public void addCompany(String company){
-        companies.add(company);
-    }
+    /*overloaded method that with no parameters that allows an individual to submit him or herself.
+     * With the parameter of company, a company can submit multiple attendees at once.
+     * All variables are
+     */
 
-/*overloaded method that with no parameters that allows an individual to submit him or herself.
-* With the parameter of company, a company can submit multiple attendees at once.
-* All variables are
- */
     public void register() {
         // if the number of attendees made is under 100, take input. otherwise quit
-        if(aIDcounter<100){
+        if (aIDcounter < 99) {
             Scanner scan = new Scanner(System.in);
-            int company = scan.nextInt();
-            int counter = companiescount.get(company);
-            if(counter<10) {
+            System.out.println("give company ID");
+            int companyID = scan.nextInt();
+            int counter = companiescounter[companyID];
+            if (counter < 10) {
+                System.out.println("Give first name");
                 String first = scan.next();
+                System.out.println("Give last name");
                 String last = scan.next();
-                String allergies = scan.nextLine();
+                System.out.println("Give allergies");
+                String allergies = scan.next();
                 int aID = aIDcounter;
-                attendees.add(new Attendee(first, last, company, allergies, aID));
-                companiescount.set(company,counter+1);
+                attendees.add(new Attendee(first, last, companyID, allergies, aID));
+                companiescounter[companyID]++;
                 aIDcounter++;
-            }
-            else System.out.println("Each Company can only submit 10 attendees");
-        }
-        else System.out.println("REGISTRATION CLOSED");
+            } else System.out.println("Each Company can only submit 10 attendees");
+        } else System.out.println("REGISTRATION CLOSED");
+    }
+//Allows a given company to submit multiple attendees at once, sort of
+    public void register(int company) {
+        if (aIDcounter<99) {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("give number of applicants");
+            int counter = scan.nextInt();
+            if (counter + companiescounter[company] <=10) {
+                for(int i=0;i<counter;i++) {
+                    System.out.println("Enter info for attendee#"+(i+1));
+                    System.out.println("Give first name");
+                    String first = scan.next();
+                    System.out.println("Give last name");
+                    String last = scan.next();
+                    System.out.println("Give allergies");
+                    String allergies = scan.next();
+                    int aID = aIDcounter;
+                    attendees.add(new Attendee(first, last, company, allergies, aID));
+                    companiescounter[company]++;
+                    aIDcounter++;
+                }
+            }else System.out.println("Maximum number of attendees per company is 10");
+        }else System.out.println("REGISTRATION CLOSED");
+    }
+
+
+    public static void main(String[] args) {
+        Banquet b1 = new Banquet();
+        b1.register();
     }
 }
